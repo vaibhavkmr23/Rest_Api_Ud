8d6593a8486952db60210660a3e3479b663a3b3a
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-23 15:28:18 +0530

    Rest APIs,Clients & CORS Errors

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const express = require('express');
 
 const app = express();
 
+const feedRoutes = require('./routes/feed');
+
 const bodyParser = require('body-parser');
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // for application/json format
 
-const feedRoutes = require('./routes/feed');
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-AlLow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow_Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use('/feed',feedRoutes);
 
