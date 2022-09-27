6eba98910fed7745944397e4f492db2a330cef65
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-27 11:01:02 +0530

    Updating Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs =require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
@@ -35,7 +38,7 @@ exports.createPost = (req, res, next) => {
         throw error;
     }
     // const imageUrl = req.file.path; // doesnt shows pic in windows use bottom one
-    const imageUrl = req.file.path.replace("\\" ,"/");
+    const imageUrl = req.file.path.replace("\\", "/");
     const title = req.body.title;
     const content = req.body.content;
 
@@ -83,4 +86,56 @@ exports.getPost = (req, res, next) => {
             }
             next(err);
         })
+}
+
+exports.updatePost = (req, res, next) => {
+    const postId = req.params.postId;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Failed..Entered Data is Incorrect!!');
+        error.statusCode = 422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if (req.file) {
+        imageUrl = req.file.path.replace("\\", "/");
+    }
+    if (!imageUrl) {
+        const error = new Error("No file Picked!");
+        error.statusCode = 422;
+        throw error;
+    }
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error("Could not find Post.");
+                error.statusCode = 404;
+                throw error;
+            }
+            if(imageUrl !== post.imageUrl){
+                clearImage(post.imageUrl);
+            }
+            post.title = title;
+            post.imageUrl = imageUrl,
+            post.content = content;
+            return post.save();
+        })
+        .then(result =>{
+            res.status(200).json({message: "Post Updated Successfully!", post: result})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
+// Helper function to delete image from file
+
+const clearImage = filePath =>{
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 }
