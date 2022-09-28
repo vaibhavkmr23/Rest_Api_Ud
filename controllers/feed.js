3b939d6ba2b066053a6ab500c52921cea91e9193
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-28 17:50:28 +0530

    Connecting Posts & Users

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -52,20 +53,26 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path.replace("\\", "/");
     const title = req.body.title;
     const content = req.body.content;
-
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: {
-            name: 'Vaibhav'
-        },
+        creator: req.userId
     });
-    post.save()
+    post.save().then(result => {
+        return User.findById(req.userId)
+    })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
         .then(result => {
             res.status(201).json({
                 message: 'Post Created Successfully!',
-                post: result
+                post: post,
+                creator: { _id: creator._id, name: creator.name}
             })
         })
         .catch(err => {
