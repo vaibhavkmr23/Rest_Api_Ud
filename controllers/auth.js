daeaf0f5f054271b14cd542b2b4b47996e419ae7
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-28 12:48:18 +0530

    Logging In & Creating Json Web Tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -55,7 +56,11 @@ exports.login = (req, res, next) => {
                 error.statusCode = 401;
                 throw error;
             }
-            res.status(200).json({ message: " Successfully LoggedIn" });
+            const token = jwt.sign({
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            }, 'somesupersecretsecretkey', { expiresIn: '1h' })
+            res.status(200).json({ token: token, userId: loadedUser._id.toString() });
         })
         .catch(err => {
             if (!err.statusCode) {
--- routes/auth.js
+++ routes/auth.js
@@ -27,6 +27,6 @@ router.put('/signup', [
         .isEmpty()
 ], authController.signup);
 
-router.post('/login');
+router.post('/login', authController.login);
 
 module.exports = router;
