91d4ecf6e9b6a05a7c764776d2df40ca520169f0
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-28 15:40:52 +0530

    Using and Validating Token

--- routes/feed.js
+++ routes/feed.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const { body } = require('express-validator/check');
 
+const isAuth = require('../middleware/is-auth');
+
 const router = express.Router();
 
 const feedController = require('../controllers/feed');
 
 // Get /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post('/post',
