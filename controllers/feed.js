2e6d5d822f7eda2a9022af4c29942655ef9eda4a
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-29 18:44:44 +0530

    Fixing a Bug- The Missing Username

--- controllers/feed.js
+++ controllers/feed.js
@@ -16,7 +16,9 @@ exports.getPosts = async (req, res, next) => {
     try {
         // Use of await where used then function
         totalItems = await Post.find().countDocuments()
-        const posts = await Post.find().skip((currentPage - 1) * perPage)
+        const posts = await Post.find()
+            .populate('creator')
+            .skip((currentPage - 1) * perPage)
             .limit(perPage);
         res.status(200).json({
             message: "Fetched posts successfully!!",
@@ -59,7 +61,7 @@ exports.createPost = async (req, res, next) => {
         const user = await User.findById(req.userId)
         user.posts.push(post);
         await user.save();
-        io.getIO().emit('posts', { action: 'create', post: post });
+        io.getIO().emit('posts', { action: 'create', post: { ...post._doc, creator: { _id: req.userId, name: user.name } } });
         res.status(201).json({
             message: 'Post Created Successfully!',
             post: post,
